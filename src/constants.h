2c78c0c7db7a70d3c61030f0f3847b33b65c571b
Kamil Dybek <kam.dyb1@gmail.com>
2024-07-15 13:10:58 +0200

    made constants inline

--- src/constants.h
+++ src/constants.h
@@ -2,12 +2,12 @@
 #define SNEK_CONSTANTS_H
 
 
-constexpr short g_mapWidth = 50;
-constexpr short g_mapHeight = 30;
-constexpr char g_backgroundChar = ' ';
-constexpr char g_borderChar = '#';
-constexpr char g_fruitChar = '@';
-constexpr char g_snekHead = 's';
+constexpr inline short g_mapWidth = 50;
+constexpr inline short g_mapHeight = 30;
+constexpr inline char g_backgroundChar = ' ';
+constexpr inline char g_borderChar = '#';
+constexpr inline char g_fruitChar = '@';
+constexpr inline char g_snekHead = 's';
 
 
 #endif // SNEK_CONSTANTS_H
